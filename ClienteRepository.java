7532b8ed65ec992b4bf73569edb057e120456142
ander3324 <ander3324@gmail.com>
2021-04-27 21:37:03 -0300

    Cliente: repositorio  añadido.

--- ClienteRepository.java
+++ ClienteRepository.java
@@ -1,4 +1,18 @@
+import java.util.List;
+import java.util.ArrayList;
 public class ClienteRepository {
 
+  public List<Cliente> clientes;
   
+  public ClienteRepository() {
+    clientes = new ArrayList<>(); //Inferencia de tipos
+
+    clientes.add(new Cliente(1, "Díaz", "Jorgito"));
+    clientes.add(new Cliente(2, "Toledo", "Lucas"));
+    clientes.add(new Cliente(3, "Hageleit", "El Flaco"));
+  } 
+
+  public List<Cliente> getClientes() {
+    return clientes;
+  }
 }
