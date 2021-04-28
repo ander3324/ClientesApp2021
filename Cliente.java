cc212fb3e852cfe9511df026407d58cedf30e329
ander3324 <ander3324@gmail.com>
2021-04-27 21:16:57 -0300

    Model: Clase Cliente añadida.

---
+++ Cliente.java
@@ -0,0 +1,32 @@
+public class Cliente {
+  
+  private int numero;
+  private String apellido;
+  private String nombre;
+  
+  public Cliente() {}
+
+  public void setNumero(int numero) {
+    this.numero = numero;
+  }
+
+  public int getNumero() {
+    return this.numero;
+  }
+
+  public void setApellido(String apellido) {
+    this.apellido = apellido;
+  }
+
+  public int getApellido() {
+    return this.apellido;
+  }
+
+  public void setNombre(String nombre) {
+    this.nombre = nombre;
+  }
+
+  public int getNombre() {
+    return this.nombre;
+  }
+}
